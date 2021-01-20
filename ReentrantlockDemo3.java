b41470382d4701f7dcd583d456617bdb0f35a420
bhargav-kommu <bhargavkommu@gmail.com>
2021-01-20 16:33:19 +0530

    Create ReentrantlockDemo3.java

---
+++ ReentrantlockDemo3.java
@@ -0,0 +1,15 @@
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantlockDemo3 {
+
+}
+
+
+class MyThread14 extends Thread{
+	
+	static ReentrantLock  l = new ReentrantLock();
+	public void run() {
+	//need to update this file	
+	}
+}
